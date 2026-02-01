17d9c9880564c11f749132dab4e89269a2f5b7ca
rajneeshmahala <rajneeshmahala@gmail.com>
2026-02-01 23:54:45 +0530

    apk gradil

--- android-app/app/src/main/java/com/devx/app/MainActivity.kt
+++ android-app/app/src/main/java/com/devx/app/MainActivity.kt
@@ -3,8 +3,8 @@ package com.devx.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.devx.app.ui.HomeScreen
 import com.devx.app.ui.DevXTheme
+import com.devx.app.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
