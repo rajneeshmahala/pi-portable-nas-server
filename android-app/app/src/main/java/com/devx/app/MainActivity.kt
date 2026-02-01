386b36dbff7a8a3761c25b325bb7c2f07253b618
rajneeshmahala <rajneeshmahala@gmail.com>
2026-02-02 00:20:37 +0530

    code added az pipeline

--- android-app/app/src/main/java/com/devx/app/MainActivity.kt
+++ android-app/app/src/main/java/com/devx/app/MainActivity.kt
@@ -3,12 +3,11 @@ package com.devx.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.devx.app.ui.DevXTheme
 import com.devx.app.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { DevXTheme { HomeScreen() } }
+        setContent { HomeScreen() }
     }
 }
